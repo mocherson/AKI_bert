362ed527817645c7f3f9eff6a07484c412ac0c96
Chengsheng Mao <chengsheng.mao@northwestern.edu>
2021-03-08 01:23:03 -0600

    update args

--- model.py
+++ model.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 from pytorch_transformers import BertPreTrainedModel, BertModel
 
 class BertForDocClassification(BertPreTrainedModel):
-    def __init__(self, config, dropout_prob=0):
+    def __init__(self, config, pooling='max' ,dropout_prob=0):
         super(BertForDocClassification, self).__init__(config)
         self.num_labels = config.num_labels
+        self.pooling = pooling
 
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(dropout_prob)
@@ -17,10 +18,11 @@ class BertForDocClassification(BertPreTrainedModel):
 
         self.init_weights()
 
-    def forward(self, input_ids, max_seqs_per_doc=150, token_type_ids=None, attention_mask=None, labels=None,
+    def forward(self, input_ids, max_seqs_per_doc=200, token_type_ids=None, attention_mask=None, labels=None,
                 position_ids=None, head_mask=None):
         dev = next(self.parameters()).device
         n_sentences=[len(x) for x in input_ids]
+        print(n_sentences)
         if self.training:
             for i, x in enumerate(input_ids):
                 if len(x)>max_seqs_per_doc:
@@ -42,16 +44,18 @@ class BertForDocClassification(BertPreTrainedModel):
 #         pooled_output = self.bn(pooled_output)
 
         pooled_output = self.dropout(pooled_output)
-#         pooled_output = torch.stack([t.max(0)[0] for t in pooled_output.split(n_sentences)])
-#         pooled_output = torch.stack([t.mean(0) for t in pooled_output.split(n_sentences)])
+        if self.pooling=='max':
+            pooled_output = torch.stack([t.max(0)[0] for t in pooled_output.split(n_sentences)])
+        elif self.pooling=='avg':
+            pooled_output = torch.stack([t.mean(0) for t in pooled_output.split(n_sentences)])
         
         logits = self.classifier(pooled_output)
-#         logits = F.softmax(logits,dim=1)
+        prob = F.softmax(logits,dim=1)
         
-        prob = torch.stack([t.mean(0) for t in logits.split(n_sentences)])
+#         prob = torch.stack([t.mean(0) for t in logits.split(n_sentences)])
 #         prob = torch.cat([1-prob,prob], dim=1)
 
-        outputs = (prob, pooled_output) + outputs[2:]  # add hidden states and attention if they are here
+        outputs = (logit, prob, pooled_output) + outputs[2:]  # add hidden states and attention if they are here
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
     
